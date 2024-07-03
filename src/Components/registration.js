95ae6e8376a91e616f284db399826909051676b2
Bhargavimadarapu22 <bhargavimadarapu22>
2024-07-03 14:04:29 +0530

    updated routing for login and registration pages

--- src/Components/registration.js
+++ src/Components/registration.js
@@ -1,22 +1,90 @@
-import React from "react";
-import './registration.css'
-function Registration()
-{
-    return  <div>
-        <div className="Registration"> 
-            <p>Sign up</p>
-            <input type="text" id="firstName" placeholder="First Name"></input>
-            <input type="text" id="lastName" className="lastName" placeholder="Last Name"></input>
-            <input type="email" id="email" placeholder="Enter email"></input>
-            <input type="password" id="password" placeholder="password"></input>
-            <input type="password" id="password" placeholder="confirm password"></input>
-            <br></br>
-            <input type="checkbox" id="agree" className="agree" value="agree"></input>
-            <label for="agree" className="agree-label">I agree to CAKE.com<a><span>Terms of Use</span></a></label>
-            <button  href="#" className="continue">Submit</button>
+import React, { useState } from "react";
+import "./registration.css";
+import { Link, useNavigate } from "react-router-dom";
+function Registration() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("fullName :", firstName + "" + lastName);
+    console.log("email :", email);
+    console.log("password :", password);
+    navigate("/login");
+  };
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="Registration">
+          <p>Sign up</p>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="First Name"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+          <input
+            type="text"
+            id="lastName"
+            className="lastName"
+            placeholder="Last Name"
+            required
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          ></input>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            id="password"
+            placeholder="Confirm Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <br></br>
+          {/* <input
+            type="checkbox"
+            id="agree"
+            className="agree"
+            value="agree"
+            required
+          /> */}
+          {/* <label for="agree" className="agree-label">
+            I agree to CAKE.com   
+            <a href="">
+              <span>Terms of Use</span>
+            </a>
+          </label> */}
+          <p>
+            Already have an Account ! <Link to="/login">Go to Login</Link>
+          </p>
+
+          <button type="submit" className="continue">
+            Submit
+          </button>
         </div>
-   
-  </div>
+      </form>
+    </div>
+  );
 }
 
 export default Registration;
